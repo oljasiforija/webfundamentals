556e76241f1564f1ec34c80444867efa1c86e065
oljasiforija <olja_siforija@hotmail.com>
2021-07-23 11:07:02 -0500

    Create w1d5.js

---
+++ algos/W1D5/w1d5.js
@@ -0,0 +1,35 @@
+// var fruit1 = "apples";
+// var fruit2 = "oranges";
+
+// fruit2 = fruit1;
+
+// console.log(fruit2 + " and " + fruit1);
+
+// var fruit1 = "apples";
+// var fruit2 = "oranges";
+
+// var temp = fruit1; // temp is a common name for this
+// fruit1 = fruit2;
+// fruit2 = temp;
+
+// console.log(fruit2 + " and " + fruit1);
+
+// var start = 0;
+// var end = 12;
+    
+// while(start < end) {
+//     console.log("start: " + start + ", end: " + end);
+//     start += 2;
+//     end -= 2;
+// }
+
+function reverse(arr){
+    var arr = [1, 2, 3, 4, 5] 
+    for(var left=0; left<arr.length/2; left++){
+    var placeholder = arr[i];
+    var position = arr.length - i - 1;
+    arr[i] = position[position];
+    arr[i] = position;}
+}
+reverse(arr);
+console.log(arr);
